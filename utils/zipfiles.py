3b8fce46825832a6e2d0afa1d3c7274b45287179
calba@CAPvoyager <calba@tid.es>
2018-09-08 11:19:59 +0200

    Mejorada la excepción

--- utils/zipfiles.py
+++ utils/zipfiles.py
@@ -6,9 +6,8 @@ def unzipfile(filename):
     try:
         zhandle = zf.ZipFile(filename)
     except BaseException as exc:
-        print("Algo malo ha pasado. ")
+        print("Algo malo ha pasado. %s: %s " % (type(exc), exc))
         exit(1)
 
-    infoList = [ x.filename for x in zhandle.infolist()]
+    infoList = [x.filename for x in zhandle.infolist()]
     return zhandle, infoList
-
