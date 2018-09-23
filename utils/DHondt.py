adeb669bf717e06b1b1d92d399ba5991df202a20
cap@CASA <cesar.alba@gmail.com>
2018-09-23 14:14:34 +0200

    Arreglado bug para votacion del 100% a una lista
    Detectado y corregido bug en tratamiento de parametro de voto en blanco

--- utils/DHondt.py
+++ utils/DHondt.py
@@ -1,11 +1,9 @@
-
 import pandas as pd
 import numpy as np
 from collections import Counter
 
 
 def DHondt(fila, votos='votCand', numescs=('datosTerr', 'numEscs'), votBlanco=('datosTerr', 'votBlanco'), umbral=0.0):
-
     if isinstance(fila, pd.core.series.Series):
         if votos in fila.index:
             Dvotos = fila[votos]
@@ -23,10 +21,10 @@ def DHondt(fila, votos='votCand', numescs=('datosTerr', 'numEscs'), votBlanco=('
 
         if isinstance(votBlanco, (int, np.uint32, np.uint64)):
             VvotBlanco = votBlanco
-        elif numescs in fila.index:
+        elif votBlanco in fila.index:
             VvotBlanco = fila[votBlanco]
         else:
-            print("DHondt: clave '%s' para numero de votos en blanco no está en fila." % numescs)
+            print("DHondt: clave '%s' para numero de votos en blanco no está en fila." % votBlanco)
             return None
     else:
         raise TypeError("Esperaba una Serie (fila procedente del Dataframe")
@@ -42,11 +40,15 @@ def DHondt(fila, votos='votCand', numescs=('datosTerr', 'numEscs'), votBlanco=('
     elected = cocientes[:VnumEscs]
 
     ultEleg = cocientes[VnumEscs - 1]
-    primNoEleg = cocientes[VnumEscs]
-    difUltEsc = ((ultEleg[2] - primNoEleg[2]) * primNoEleg[1])
+    if len(cocientes) > VnumEscs:
+        primNoEleg = cocientes[VnumEscs]
+        difUltEsc = ((ultEleg[2] - primNoEleg[2]) * primNoEleg[1])
+    else:
+        primNoEleg = None
+        difUltEsc = None
 
     dictElegidos = dict(Counter([x[0] for x in elected]))
-    costeAsiento = {('costeAsiento', x): umbVotos[x]/dictElegidos[x] for x in dictElegidos}
+    costeAsiento = {('costeAsiento', x): umbVotos[x] / dictElegidos[x] for x in dictElegidos}
     sinEsc = {x: np.uint32(0) for x in actVotos.index if x not in dictElegidos}
     votosSinEsc = {('votosSinAsiento', x): actVotos[x] if x not in dictElegidos else 0 for x in actVotos.index}
     dictElegidos.update(sinEsc)
@@ -57,8 +59,15 @@ def DHondt(fila, votos='votCand', numescs=('datosTerr', 'numEscs'), votBlanco=('
     votosUmbral = pd.Series({('votosUmbral', 'pasa'): umbVotos.sum(), ('votosUmbral', 'noPasa'): noPasaUmbral},
                             dtype=np.uint64)
     ultimoSi = pd.Series({('ultElegido', 'Partido'): ultEleg[0], ('ultElegido', 'Posicion'): ultEleg[1]})
-    primeroNo = pd.Series({('primNoElegido', 'Partido'): primNoEleg[0], ('primNoElegido', 'Posicion'): primNoEleg[1]})
-    diferUltimo = np.ceil(pd.Series({('difUltEleg', 'votos'): difUltEsc}))
+
+    if primNoEleg is None:
+        primeroNo = pd.Series({('primNoElegido', 'Partido'): None, ('primNoElegido', 'Posicion'): None})
+        diferUltimo = pd.Series({('difUltEleg', 'votos'): None})
+    else:
+        primeroNo = pd.Series(
+            {('primNoElegido', 'Partido'): primNoEleg[0], ('primNoElegido', 'Posicion'): primNoEleg[1]})
+        diferUltimo = np.ceil(pd.Series({('difUltEleg', 'votos'): difUltEsc}))
+
     costeAsientoS = pd.Series(costeAsiento)
     votosSinEscS = pd.Series(votosSinEsc)
 
