e57eed82a6d86332cb4efc4935906360672ac198
cap@CASA <cesar.alba@gmail.com>
2019-03-10 12:03:19 +0100

    Calculo de Matrices Recursivo

--- seccCensales/matrAdyacencia.py
+++ seccCensales/matrAdyacencia.py
@@ -1,20 +1,37 @@
 from collections.abc import Iterable
-from itertools import product, chain
+from itertools import product
 
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+from scipy.sparse import dok_matrix
+
+secNIV = ['CCAA', 'PRO', 'MUN', 'DIS', 'SEC']
+# secNIV = ['CCAA', 'PRO']
+
+resultBase = {'CCAA': {'claveAgr': 'CCA', 'extraCols': ['NCA', 'nCCA']},
+              'PRO': {'claveAgr': 'CPRO', 'extraCols': ['CCA', 'NCA', 'NPRO', 'nCCA', 'nCPRO'], 'nSUP': 'CCAA'},
+              'MUN': {'claveAgr': 'CUMUN',
+                      'extraCols': ['CCA', 'CPRO', 'CMUN', 'NCA', 'NPRO', 'NMUN', 'nCCA', 'nCPRO', 'nCMUN', 'nCUMUN'],
+                      'nSUP': 'PRO'},
+              'DIS': {'claveAgr': 'CUDIS',
+                      'extraCols': ['CCA', 'CPRO', 'CMUN', 'CDIS', 'CUMUN', 'NCA', 'NPRO', 'NMUN', 'nCCA', 'nCPRO',
+                                    'nCMUN', 'nCDIS', 'nCUMUN', 'nCUDIS'], 'nSUP': 'MUN'},
+              'SEC': {'claveAgr': 'CUSEC',
+                      'extraCols': ['CCA', 'CPRO', 'CMUN', 'CDIS', 'CUMUN', 'CUDIS', 'NCA', 'NPRO', 'NMUN', 'nCCA',
+                                    'nCPRO', 'nCMUN', 'nCDIS', 'nCUMUN', 'nCUDIS', 'nCUSEC'], 'nSUP': 'DIS'}
+              }
 
 
 def leeContornoSeccionesCensales(fname):
-    result = gpd.read_file(fname)
+    baregdf = gpd.read_file(fname)
 
     # Pasada a limpio (hasta que lo arreglen en INE, reportado).
     # Esto es feo pero dado que sólo existe un fichero de contornos da un poco igual
-    result.loc[result.CCA == "16", 'NCA'] = "País Vasco"
-    result.loc[result.CPRO == "20", 'NPRO'] = "Gipuzkoa"
-    result.loc[result.CUMUN == "20069", 'NMUN'] = "Donostia-San Sebastián"
-    result.loc[result.CUMUN == "28092", 'NMUN'] = "Móstoles"
+    baregdf.loc[baregdf.CCA == "16", 'NCA'] = "País Vasco"
+    baregdf.loc[baregdf.CPRO == "20", 'NPRO'] = "Gipuzkoa"
+    baregdf.loc[baregdf.CUMUN == "20069", 'NMUN'] = "Donostia-San Sebastián"
+    baregdf.loc[baregdf.CUMUN == "28092", 'NMUN'] = "Móstoles"
 
     # Control de calidad de etiquetas (ya corregido en       leeContornoSeccionesCensales
     # Encuentra nombres mal puestos
@@ -27,20 +44,26 @@ def leeContornoSeccionesCensales(fname):
     # reMun = gdf.groupby('CUMUN').apply(lambda x: x['NMUN'].value_counts())
     # reMun[(reMun.droplevel(1,axis=0).index.duplicated(keep=False))]
 
+    result = creaNumCols(baregdf, ['CCA', 'CPRO', 'CMUN', 'CDIS', 'CSEC', 'CUMUN', 'CUDIS', 'CUSEC'])
+
     result['numcell'] = 1  # Se usa para contar secciones para agrupación mayor
 
     return result
 
 
+def checkList(obj):
+    return isinstance(obj, Iterable) and not isinstance(obj, str)
+
+
 def agrupaContornos(df, claveAgr, extraCols=None):
-    merged = df.dissolve(by=claveAgr, aggfunc=sum)
+    auxClave = claveAgr if checkList(claveAgr) else [claveAgr]
+    merged = df[auxClave + ['geometry', 'numcell']].dissolve(by=claveAgr, aggfunc=sum)
 
     # Si no hay que poner etiqueta, our job is done
     if extraCols is None:
         return merged
 
-    auxClave = claveAgr if isinstance(claveAgr, Iterable) else [claveAgr]
-    auxExtra = extraCols if isinstance(extraCols, Iterable) else [extraCols]
+    auxExtra = extraCols if checkList(extraCols) else [extraCols]
 
     resto = df[auxClave + auxExtra].drop_duplicates().set_index(auxClave)
 
@@ -50,7 +73,7 @@ def agrupaContornos(df, claveAgr, extraCols=None):
 
 
 def creaNumCols(df, cols):
-    auxCols = cols if isinstance(cols, Iterable) else [cols]
+    auxCols = cols if checkList(cols) else [cols]
     result = df.reset_index()
 
     indexCol = None
@@ -74,7 +97,7 @@ def creaMatriz(df, clave=None, matricesMR=None):
     :param matricesMR: diccionario de pares { columnaDF:matrizADJ } para reducir cálculos. Ej: 2 provincias no pueden
                        ser adyacentes si sus respectivas CCAA no lo son
     :return: dataframe con la matriz resultante
-     
+
     """
     dimdf = len(df)
     matriz = np.zeros_like(np.arange(dimdf * dimdf).reshape((dimdf, dimdf)), dtype=bool)
@@ -123,25 +146,25 @@ def creaMatriz(df, clave=None, matricesMR=None):
 
 
 def creaMatrizJoblib(df, clave=None, matricesMR=None, JLconfig=None):
-
     from joblib import Parallel, delayed
-    from scipy.sparse import coo_matrix, dok_matrix
+    from scipy.sparse import dok_matrix
 
     if JLconfig is None:
         JLconfig = {}
     configParallel = {'verbose': 20}
     # TODO: Control de calidad con los parámetros
-    configParallel['n_jobs'] = JLconfig.get('nproc',2)
-    configParallel['prefer'] = JLconfig.get('joblibmode','threads')
+    configParallel['n_jobs'] = JLconfig.get('nproc', 2)
+    configParallel['prefer'] = JLconfig.get('joblibmode', 'threads')
 
     dimdf = len(df)
     # matriz = np.zeros_like(np.arange(dimdf * dimdf).reshape((dimdf, dimdf)), dtype=bool)
 
     idx = df.index.to_list()
     ridx = list(range(dimdf))
+
     # estads = {'simetria': 0, 'matriz': 0, 'inters': 0}
 
-    def checkAdjacency(ix,iy):
+    def checkAdjacency(ix, iy):
         if iy < ix:
             return None
 
@@ -164,20 +187,142 @@ def creaMatrizJoblib(df, clave=None, matricesMR=None, JLconfig=None):
         g1 = d1.geometry
 
         if g0.intersects(g1):
-            return (ix,iy)
+            return (ix, iy)
 
-    resultJL = Parallel(**configParallel)(delayed(checkAdjacency)(x,y) for x,y in product(ridx, ridx))
+    resultJL = Parallel(**configParallel)(delayed(checkAdjacency)(x, y) for x, y in product(ridx, ridx))
 
-    matriz = dok_matrix((dimdf,dimdf), dtype=bool)
+    matriz = dok_matrix((dimdf, dimdf), dtype=bool)
     for x in resultJL:
         if x is None:
             continue
         print(x)
-        matriz[x[0],x[1]]=True
-        matriz[x[1],x[0]]=True
+        matriz[x[0], x[1]] = True
+        matriz[x[1], x[0]] = True
 
     targNames = df[clave] if clave else df.index
 
     result = pd.DataFrame(matriz.todense(), index=targNames, columns=targNames)
 
     return result
+
+
+def preparaAgrupacionConts(df):
+    """
+
+    :param df:
+    :return:
+    """
+    result = {k: resultBase[k] for k in secNIV}
+    # Prepara los agregados de contornos y datos auxiliares
+    for k in secNIV:
+        aux = agrupaContornos(df, claveAgr=result[k]['claveAgr'], extraCols=result[k]['extraCols'])
+        aux['idx'] = range(len(aux))
+
+        if result[k].get('nSUP', None):
+            supLevel = result[k]['nSUP']
+            kSupLevel = result[supLevel]['claveAgr']
+
+            result[supLevel]['sup2k'] = aux.reset_index().groupby(kSupLevel)[result[k]['claveAgr']].apply(list)
+
+        result[k]['contAgr'] = aux
+
+    return result
+
+
+def creaMatrizRec(contAggsEst, listaNiv):
+    """
+    A partir de un geodataframe devuelve un dataframe con la matriz de adyacencias entre sus filas
+    :param df: geodataframe (geopandas)
+    :param clave: columna del dataframe original que se va a usar como clave (filas y columnas del DF resultante)
+    :param matricesMR: diccionario de pares { columnaDF:matrizADJ } para reducir cálculos. Ej: 2 provincias no pueden
+                       ser adyacentes si sus respectivas CCAA no lo son
+    :return: dataframe con la matriz resultante
+
+    """
+
+    resMat = encuentraVecinos(listaNiv[0], listaNiv[1:], contAggsEst, contAggsEst[listaNiv[0]]['contAgr'].index,
+                              contAggsEst[listaNiv[0]]['contAgr'].index)
+
+    resDF = vecinos2DF(resMat, contAggsEst)
+
+    return resDF
+
+
+def encuentraVecinos(cat, restoCats, datos, idx1, idx2, intra=True):
+    """
+
+    :param cat:
+    :param restoCats:
+    :param datos:
+    :param idx1:
+    :param idx2:
+    :param intra:
+    :return:
+    """
+    result = {k: [] for k in ([cat] + restoCats)}
+
+    print("encuentraVecinos: cat %s rem cats: %i ( %s ) parejas: %i * %i" % (
+        cat, len(restoCats), restoCats, len(idx1), len(idx2)))
+    for kx, ky in product(idx1, idx2):
+
+        dx = datos[cat]['contAgr'].loc[kx]
+        dy = datos[cat]['contAgr'].loc[ky]
+
+        if intra and (dy.idx < dx.idx):
+            continue
+
+        gx = dx.geometry
+        gy = dy.geometry
+
+        if (kx == ky) or gx.intersects(gy):
+            result[cat].append((dx['idx'], dy['idx']))
+            if (kx != ky):
+                result[cat].append((dy['idx'], dx['idx']))
+
+            if restoCats:
+                deeperResult = encuentraVecinos(restoCats[0], restoCats[1:], datos, datos[cat]['sup2k'].loc[kx],
+                                                datos[cat]['sup2k'].loc[ky], intra=(kx == ky))
+
+                for k in deeperResult:
+                    result[k] += deeperResult[k]
+
+    return result
+
+
+def vecinos2DF(resVecinos, datos):
+    """
+    A partir del resultado de encuentraVecinos crea un diccionario de dataframes dispersos con las adyacencias en el
+    nivel correspondiente
+
+    :param resVecinos: resultado de encuentraVecinos
+    :param datos: resultado de preparaAgrupacion
+    :return: diccionario de DF dispersos. Los DF corresponden a las adyacencias al nivel indicado y tienen como índice
+    el de los DF en datos.
+    """
+    auxMat = {k: dok_matrix((len(datos[k]['contAgr']), len(datos[k]['contAgr'])), dtype=bool) for k in resVecinos}
+    result = dict()
+
+    for k, sols in resVecinos.items():
+        print(k, len(sols))
+        for s in sols:
+            auxMat[k][s[0], s[1]] = True
+        result[k] = pd.SparseDataFrame(auxMat[k], index=datos[k]['contAgr'].index, columns=datos[k]['contAgr'].index,
+                                       dtype=bool, default_fill_value=False)
+
+    return result
+
+
+def setDFLabels(df, datos, clave, etiqueta):
+    """
+    Cambia las etiquetas (index y columnas) de un DF (ojo, creaMatrizRec devuelve un dict de DFs) por otras que puedan
+    ser más claras (nombres en lugar de números)
+    :param df: DF a modificar
+    :param datos: resultado de preparaAgrupacion
+    :param clave: Clave para sacar el DF correspondiente en datos (sólo tiene sentido para 'CCAA','PRO' y quizás 'MUN')
+    :param etiqueta: Columna que contiene la etiqueta de la agregación (resp. 'NCA','NPRO','NMUN'.
+    :return: DF con los indices (vert y horiz) cambiados
+
+
+    """
+    return df.set_axis(datos[clave]['contAgr'][etiqueta], axis=1, inplace=False).set_axis(
+        datos[clave]['contAgr'][etiqueta], axis=0, inplace=False)
