2d5f2dafa30f148b1f222ee62d3a10ce3c2473db
cap@CASA <cesar.alba@gmail.com>
2020-06-27 23:59:31 +0200

    Reformatting

--- seccCensales/operDF.py
+++ seccCensales/operDF.py
@@ -3,14 +3,14 @@ manipSecCensales = [{'2numeric': {'prefix': 'n',
                     {'concat': {'NUT1': ['CNUT0', 'CNUT1'],
                                 'NUT2': ['CNUT0', 'CNUT1', 'CNUT2'],
                                 'NUT3': ['CNUT0', 'CNUT1', 'CNUT2', 'CNUT3']}
-                     }
+                     },
                     ]
 
 validaciones = [['CCA', 'NCA'], ['CCA', 'CNUT0'], ['CCA', 'NUT1'], ['CCA', 'NUT2'], ['CCA', 'CNUT1'], ['CCA', 'CNUT2'],
 
                 ['CPRO', 'NPRO'], ['CPRO', 'NCA'], ['NPRO', 'NCA'], ['CPRO', 'CNUT0'], ['CPRO', 'NUT1'],
                 ['CPRO', 'NUT2'], ['NUT3', 'CPRO'], ['NUT3', 'NPRO'], ['CPRO', 'CNUT1'],
-               ['CPRO', 'CNUT2'],
+                ['CPRO', 'CNUT2'],
 
                 ['CUMUN', 'NMUN'], ['CUMUN', 'CMUN'], ['CUMUN', 'NCA'], ['CUMUN', 'NPRO'], ['CUMUN', 'NMUN'],
 
